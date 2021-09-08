0f7bfd94ac56073a1a73c6497e0ca84cb1e10e90
Vijay-sharma989 <vs489119@gmail.com>
2021-09-08 19:58:53 +0530

    my app first commit

---
+++ users/models.py
@@ -0,0 +1,22 @@
+import PIL
+from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image,UnidentifiedImageError
+
+# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg',upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+
+    def save(self,*args, **kwargs):
+        super(Profile,self).save(*args, **kwargs)
+        img = Image.open(self.image.path)
+        rgb_im = img.convert('RGB')
+        if rgb_im.height > 300 or rgb_im.width > 300:
+            output_size = (300,300)
+            rgb_im.thumbnail(output_size)
+            rgb_im.save(self.image.path)
